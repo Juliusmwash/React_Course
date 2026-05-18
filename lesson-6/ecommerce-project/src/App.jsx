233986c262f69e55933e009dd7a955352b76616b
Juliusmwash <cutejuliusmwash@gmail.com>
2026-05-19 02:37:28 +0300

    Create multipy p;ages using react-router

--- lesson-6/ecommerce-project/src/App.jsx
+++ lesson-6/ecommerce-project/src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { Route, Routes, } from 'react-router'
 import { HomePage } from './assets/pages/HomePage'
 import { Header } from './assets/pages/Header'
 
@@ -6,10 +6,26 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Header />
-      <HomePage />
-    </>
+    <Routes>
+      {/* <Route path="/" element={<HomePage />} /> */}
+      <Route
+        index // path="/" is the same as index
+        element={
+          <>
+            <Header />
+            <HomePage />
+          </>
+        }
+      />
+      <Route
+        path="/products"
+        element={<h1>Products Page</h1>}
+      />
+       <Route
+        path="/contact"
+        element={<h1>Contact Page</h1>}
+      />
+    </Routes>
   )
 }
 
