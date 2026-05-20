4c020037c745c092239f50cda2a5e1a7df71fc2b
Juliusmwash <cutejuliusmwash@gmail.com>
2026-05-20 23:58:59 +0300

    Fetch data from  the backend

--- lesson-6/ecommerce-project/src/pages/HomePage.jsx
+++ lesson-6/ecommerce-project/src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
 import { Helmet } from "react-helmet-async";
-import {products} from '../data/products.js';
+import { products } from '../data/products.js';
 
 import './HomePage.css';
 
 export function HomePage() {
+
+  fetch('http://localhost:3000/api/products')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching products:', error);
+    });
+
   return (
     <>
       <Helmet>
@@ -12,59 +22,59 @@ export function HomePage() {
           rel="icon"
           type="image/png"
           href="images/favicon/home-favicon.png"
-       />
+        />
       </Helmet>
-    
+
       <div className="home-page">
         <div className="products-grid">
-            {products.map((product) => (
-                <div className="product-container" key={product.id}>
-                    <div className="product-image-container">
-                        <img className="product-image" src={product.image} />
-                    </div>
+          {products.map((product) => (
+            <div className="product-container" key={product.id}>
+              <div className="product-image-container">
+                <img className="product-image" src={product.image} />
+              </div>
 
-                    <div className="product-name limit-text-to-2-lines">
-                        {product.name}
-                    </div>
+              <div className="product-name limit-text-to-2-lines">
+                {product.name}
+              </div>
 
-                    <div className="product-rating-container">
-                        <img
-                            className="product-rating-stars"
-                            src={`images/ratings/rating-${product.rating.stars * 10}.png`}
-                        />
-                        <div className="product-rating-count link-primary">
-                            {product.rating.count}
-                        </div>
-                    </div>
-
-                    <div className="product-price">
-                        ${(product.priceCents / 100).toFixed(2)}
-                    </div>
-
-                    <div className="product-quantity-container">
-                        <select>
-                            {[...Array(10)].map((_, index) => (
-                                <option value={index + 1} key={index}>
-                                    {index + 1}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <div className="product-spacer"></div>
-
-                    <div className="added-to-cart">
-                        <img src="images/icons/checkmark.png" />
-                        Added
-                    </div>
-
-                    <button className="add-to-cart-button button-primary">
-                        Add to Cart
-                    </button>
+              <div className="product-rating-container">
+                <img
+                  className="product-rating-stars"
+                  src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+                />
+                <div className="product-rating-count link-primary">
+                  {product.rating.count}
                 </div>
-            ))}
+              </div>
+
+              <div className="product-price">
+                ${(product.priceCents / 100).toFixed(2)}
+              </div>
+
+              <div className="product-quantity-container">
+                <select>
+                  {[...Array(10)].map((_, index) => (
+                    <option value={index + 1} key={index}>
+                      {index + 1}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="product-spacer"></div>
+
+              <div className="added-to-cart">
+                <img src="images/icons/checkmark.png" />
+                Added
+              </div>
+
+              <button className="add-to-cart-button button-primary">
+                Add to Cart
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </>
-    );
+  );
 }
