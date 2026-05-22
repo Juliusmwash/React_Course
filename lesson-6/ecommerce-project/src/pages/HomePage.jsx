9c694dc1a15a3508615771c5fdb333eeb359aaa5
Juliusmwash <cutejuliusmwash@gmail.com>
2026-05-22 21:15:50 +0300

    Add server proxy config for /api and /iimages

--- lesson-6/ecommerce-project/src/pages/HomePage.jsx
+++ lesson-6/ecommerce-project/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ export function HomePage() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products')
+    axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
       })
@@ -18,7 +18,7 @@ export function HomePage() {
         console.error('Error fetching products:', error);
       });
 
-      axios.get('http://localhost:3000/api/cart-items')
+      axios.get('/api/cart-items')
         .then((response) => {
           setCart(response.data);
         })
